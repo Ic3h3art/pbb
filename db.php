df5fb9920065b9aba2abedbd11fa5365c1877c89
Ic3h3art <35437735+Ic3h3art@users.noreply.github.com>
2020-12-04 10:34:30 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $mysqli_hostname = "localhost";
-$mysqli_user = "root";
-$mysqli_password = "";
-$mysqli_database = "pbb";
+$mysqli_user = "jborja";
+$mysqli_password = "jborja";
+$mysqli_database = "jborja";
 $conn = mysqli_connect($mysqli_hostname, $mysqli_user, $mysqli_password,$mysqli_database) 
 or die("Opps some thing went wrong");
 // mysqli_select_db($mysqli_database, $bd) or die("Opps some thing went wrong");
